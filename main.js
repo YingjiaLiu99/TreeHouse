cf76cb3d214e749db62208e87d0c1e4c15c1b65d
YingjiaLiu99 <yliu4033@gmail.com>
2024-09-06 23:26:18 -0500

    dummy main scene added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import Phaser, { Physics } from 'phaser';
 import './style.css';
 
 const sizes = {
@@ -6,11 +6,31 @@ const sizes = {
   height: 1080
 }
 
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game");
+  }
+
+  preload() {
+    this.load.image("bg", "/assets/diningRoom.jpg");
+  }
+
+  create() {    
+    let background = this.add.image(0, 0, "bg").setOrigin(0, 0);    
+    background.setDisplaySize(sizes.width, sizes.height);
+  }
+
+  update() {    
+  }
+}
+
 const config = {
   type: Phaser.WEBGL,
   width: sizes.width,
   height: sizes.height,
-  canvas: gameCanvas
+  canvas: gameCanvas,
+  physics: {},
+  scene: [GameScene]  
 }
 
 const game = new Phaser.Game(config);
