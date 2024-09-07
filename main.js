b513802768a65d028fcd7a3de741930b5b22afd4
YingjiaLiu99 <yliu4033@gmail.com>
2024-09-06 22:54:54 -0500

    basics

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-import './style.css'
+import Phaser from 'phaser';
+import './style.css';
+
+const sizes = {
+  width: 1920,
+  height: 1080
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas
+}
+
+const game = new Phaser.Game(config);
